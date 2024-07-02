ccda202812955f834f21a56c31992293d1fe0900
Jorge Paul <jserranopaul@gmail.com>
2024-07-02 20:36:15 +0200

    Avatar

--- src/components/ChatCointainer.jsx
+++ src/components/ChatCointainer.jsx
@@ -1,4 +1,5 @@
 import Markdown from 'marked-react';
+import { useOutletContext } from 'react-router-dom';
 import { Refractor, registerLanguage } from 'react-refractor';
 // Load any languages you want to use from `refractor`
 import bash from 'refractor/lang/bash';
@@ -22,10 +23,28 @@ const renderer = {
 };
 
 const ChatContainer = ({ messages }) => {
+  const { profilePic } = useOutletContext();
+
   return messages
     .filter(m => m.role !== 'system') // Filter out system messages
     .map(m => (
       <div key={m.id} className={`chat ${m.role === 'assistant' ? 'chat-start' : 'chat-end'}`}>
+        {m.role === 'user' && profilePic && (
+          <div className='chat-image avatar'>
+            <div className='w-20 rounded-full'>
+              <img
+                src={
+                  profilePic.b64_json.includes('data:image/png;base64,')
+                    ? profilePic.b64_json
+                    : `data:image/png;base64,${profilePic.b64_json}`
+                }
+                alt='User'
+                className='rounded-full shadow-lg'
+                width={50}
+              />
+            </div>
+          </div>
+        )}
         <div
           className={`chat-bubble ${
             m.role === 'assistant' ? 'chat-bubble-primary' : 'chat-bubble-secondary'
